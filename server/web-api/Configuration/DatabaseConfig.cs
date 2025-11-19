804797f78e90b88f447b9233ae879648b100f8af
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-18 21:53:46 -0300

    feat: Implementa arquivos de configurações.

---
+++ server/web-api/Configuration/DatabaseConfig.cs
@@ -0,0 +1,17 @@
+﻿using Locadora_de_Automoveis.Infraestrutura.ORM.Shared;
+
+namespace Locadora_de_Automoveis.WebAPI.Configuration;
+
+public static class DatabaseConfig
+{
+    public static bool AutoMigrateDatabase(this IHost app)
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+
+        AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        bool success = DatabaseMigrator.AutoDatabaseUpdate(dbContext);
+
+        return success;
+    }
+}
