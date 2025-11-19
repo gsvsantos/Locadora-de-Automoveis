72ddd36a7e8f7f8b54c7d905329100ce7ecdea74
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-18 23:57:34 -0300

    feat: Implementa registro de usuário em /core/application.

---
+++ server/core/application/Auth/Commands/Register/RegisterUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Core.Application.Auth.DTOs;
+using MediatR;
+
+namespace LocadoraDeAutomoveis.Core.Application.Auth.Commands.Register;
+
+public record RegisterUserRequest(
+    string UserName,
+    string FullName,
+    string Email,
+    string Password
+) : IRequest<Result<TokenResponse>>;
---
+++ server/core/application/Auth/Commands/Register/RegisterUserRequestHandler.cs
@@ -0,0 +1,68 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Core.Application.Auth.DTOs;
+using LocadoraDeAutomoveis.Core.Application.Shared;
+using LocadoraDeAutomoveis.Core.Domain.Auth;
+using LocadoraDeAutomoveis.Core.Domain.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace LocadoraDeAutomoveis.Core.Application.Auth.Commands.Register;
+
+public class RegisterUserRequestHandler(
+    UserManager<User> userManager,
+    ITokenProvider tokenProvider,
+    IUnitOfWork unitOfWork,
+    ILogger<RegisterUserRequestHandler> logger
+) : IRequestHandler<RegisterUserRequest, Result<TokenResponse>>
+{
+    public async Task<Result<TokenResponse>> Handle(
+        RegisterUserRequest request, CancellationToken cancellationToken)
+    {
+        User user = new()
+        {
+            UserName = request.UserName,
+            FullName = request.FullName,
+            Email = request.Email
+        };
+
+        try
+        {
+            IdentityResult usuarioResult = await userManager.CreateAsync(user, request.Password);
+
+            if (!usuarioResult.Succeeded)
+            {
+                IEnumerable<string> erros = usuarioResult
+                    .Errors
+                    .Select(failure => failure.Description)
+                    .ToList();
+
+                return Result.Fail(ErrorResults.BadRequestError(erros));
+            }
+
+            await userManager.AddToRoleAsync(user, "Admin");
+
+            TokenResponse? accessToken = await tokenProvider.GenerateAccessToken(user) as TokenResponse;
+
+            if (accessToken == null)
+            {
+                await unitOfWork.RollbackAsync();
+
+                return Result.Fail(ErrorResults.InternalServerError(new Exception("Failed to generate access token")));
+            }
+
+            return Result.Ok(accessToken);
+        }
+        catch (Exception ex)
+        {
+            await unitOfWork.RollbackAsync();
+
+            logger.LogError(
+                ex,
+                "An error occurred during registration. \n{@Request}.", request
+            );
+
+            return Result.Fail(ErrorResults.InternalServerError(ex));
+        }
+    }
+}
