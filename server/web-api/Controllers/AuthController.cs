5d17cd204f6f5bd53b556466595c125773f25184
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-19 04:55:25 -0300

    feat: Implementa AuthController com endpoint para registro de usuário.

---
+++ server/web-api/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Core.Application.Auth.Commands.Register;
+using LocadoraDeAutomoveis.Core.Application.Auth.DTOs;
+using LocadoraDeAutomoveis.Core.Domain.Auth;
+using LocadoraDeAutomoveis.WebAPI.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeAutomoveis.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController(IMediator mediator, SignInManager<User> signInManager) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserRequest request)
+    {
+        Result<TokenResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+}
