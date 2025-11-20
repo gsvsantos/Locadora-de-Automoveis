c10d7db8f4f1ac305e03c79e908c3f5e0f9bb9e6
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-19 22:47:42 -0300

    feat: Implementa módulo de funcionário em /infastructure/orm.

---
+++ server/infrastructure/orm/Employees/EmployeeMapper.cs
@@ -0,0 +1,34 @@
+﻿using LocadoraDeAutomoveis.Domain.Employees;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeAutomoveis.Infrastructure.Employees;
+
+public class EmployeeMapper : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.FullName)
+            .IsRequired();
+
+        builder.Property(e => e.AdmissionDate)
+            .IsRequired();
+
+        builder.Property(e => e.Salary)
+               .HasPrecision(18, 2);
+
+        builder.HasOne(t => t.Tenant)
+               .WithMany()
+               .HasForeignKey(t => t.TenantId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(u => u.User)
+               .WithOne()
+               .HasForeignKey<Employee>(e => e.UserId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(f => new { f.UserId, f.IsActive });
+    }
+}
---
+++ server/infrastructure/orm/Employees/EmployeeRepository.cs
@@ -0,0 +1,7 @@
+﻿using LocadoraDeAutomoveis.Domain.Employees;
+using LocadoraDeAutomoveis.Infrastructure.Shared;
+
+namespace LocadoraDeAutomoveis.Infrastructure.Employees;
+
+public class EmployeeRepository(AppDbContext context)
+    : BaseRepository<Employee>(context), IRepositoryEmployee;
