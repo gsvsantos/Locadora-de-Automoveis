6c89082d639ac361cb1842ed9efb048c214b54ee
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-20 01:38:28 -0300

    feat: Implementa endpoint para edição de funcionário em EmployeeController.

--- server/web-api/Controllers/EmployeeController.cs
+++ server/web-api/Controllers/EmployeeController.cs
@@ -1,19 +1,43 @@
 ﻿using FluentResults;
 using LocadoraDeAutomoveis.Application.Employees.Commands.Create;
+using LocadoraDeAutomoveis.Application.Employees.Commands.Update;
 using LocadoraDeAutomoveis.WebAPI.Extensions;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LocadoraDeAutomoveis.WebApi.Controllers;
 
 [ApiController]
 [Route("api/employee")]
+[Authorize(Roles = "Admin")]
 public class EmployeeController(IMediator mediator) : ControllerBase
 {
     [HttpPost("create")]
     public async Task<IActionResult> Create([FromBody] CreateEmployeeRequest request)
     {
         Result<CreateEmployeeResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpPut("update/{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] UpdateEmployeeRequestPartial partialRequest)
+    {
+
+        UpdateEmployeeRequest request = new(
+            id,
+            partialRequest.FullName,
+            partialRequest.AdmissionDate,
+            partialRequest.Salary
+        );
+
+        Result<UpdateEmployeeResponse> result = await mediator.Send(request);
 
         if (result.IsFailed)
         {
