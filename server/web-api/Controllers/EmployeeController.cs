f69be7989c4a4d1bfaba92b2ded4a437d6388280
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-20 00:04:19 -0300

    feat: Implementa endpoint para criação de funcionário em EmployeeController.

---
+++ server/web-api/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Application.Employees.Commands.Create;
+using LocadoraDeAutomoveis.WebAPI.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeAutomoveis.WebApi.Controllers;
+
+[ApiController]
+[Route("api/employee")]
+public class EmployeeController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> Create([FromBody] CreateEmployeeRequest request)
+    {
+        Result<CreateEmployeeResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+}
