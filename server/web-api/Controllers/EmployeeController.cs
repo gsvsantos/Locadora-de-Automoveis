3bba9e2530e926517b750172da980dbb159d685e
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-20 22:46:16 -0300

    feat: Implementa endpoint para selecionar todos os funcionários em EmployeeController.

--- server/web-api/Controllers/EmployeeController.cs
+++ server/web-api/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using FluentResults;
 using LocadoraDeAutomoveis.Application.Employees.Commands.Create;
 using LocadoraDeAutomoveis.Application.Employees.Commands.Delete;
+using LocadoraDeAutomoveis.Application.Employees.Commands.GetAll;
 using LocadoraDeAutomoveis.Application.Employees.Commands.SelfUpdate;
 using LocadoraDeAutomoveis.Application.Employees.Commands.Update;
 using LocadoraDeAutomoveis.Domain.Auth;
@@ -20,6 +21,22 @@ public class EmployeeController(IMediator mediator) : ControllerBase
     public async Task<IActionResult> Create([FromBody] CreateEmployeeRequest request)
     {
         Result<CreateEmployeeResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("GetAll")]
+    [Authorize("AdminPolicy")]
+    public async Task<IActionResult> GetAll()
+    {
+        GetAllEmployeeRequest request = new();
+
+        Result<GetAllEmployeeResponse> result = await mediator.Send(request);
 
         if (result.IsFailed)
         {
