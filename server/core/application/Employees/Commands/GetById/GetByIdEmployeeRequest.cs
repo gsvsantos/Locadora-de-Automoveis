0673e6e93184752f88db20df2389faa8d3a76ba9
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-20 23:15:40 -0300

    feat: Implementa handler de seleção de funcionário em /core/application.

---
+++ server/core/application/Employees/Commands/GetById/GetByIdEmployeeRequest.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace LocadoraDeAutomoveis.Application.Employees.Commands.GetById;
+
+public record GetByIdEmployeeRequest(Guid Id) : IRequest<Result<GetByIdEmployeeResponse>>;
---
+++ server/core/application/Employees/Commands/GetById/GetByIdEmployeeRequestHandler.cs
@@ -0,0 +1,44 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Application.Employees.Commands.GetAll;
+using LocadoraDeAutomoveis.Application.Shared;
+using LocadoraDeAutomoveis.Domain.Employees;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace LocadoraDeAutomoveis.Application.Employees.Commands.GetById;
+
+public class GetByIdEmployeeRequestHandler(
+    IRepositoryEmployee repositoryEmployee,
+    ILogger<GetByIdEmployeeRequestHandler> logger
+) : IRequestHandler<GetByIdEmployeeRequest, Result<GetByIdEmployeeResponse>>
+{
+    public async Task<Result<GetByIdEmployeeResponse>> Handle(
+        GetByIdEmployeeRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            Employee? employee = await repositoryEmployee.GetByIdAsync(request.Id);
+
+            if (employee == null)
+            {
+                return Result.Fail(ErrorResults.NotFoundError(request.Id));
+            }
+
+            GetByIdEmployeeResponse response = new(
+                 new EmployeeDto(
+                    employee.Id,
+                    employee.FullName,
+                    employee.AdmissionDate,
+                    employee.Salary
+                )
+            );
+
+            return Result.Ok(response);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error retrieving employee by ID {EmployeeId}", request.Id);
+            return Result.Fail(new Error("An error occurred while retrieving the employee."));
+        }
+    }
+}
