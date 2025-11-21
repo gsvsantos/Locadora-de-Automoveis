c24e5657c90fae8ed24fc00807b2ae9fe14ffcf0
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-21 20:26:06 -0300

    feat: Implementa módulo de grupos de veículos em /core/domain.

---
+++ server/core/domain/Groups/Group.cs
@@ -0,0 +1,10 @@
+﻿using LocadoraDeAutomoveis.Domain.Shared;
+
+namespace LocadoraDeAutomoveis.Domain.Groups;
+
+public class Group : BaseEntity<Group>
+{
+    public string Name { get; set; } = string.Empty;
+
+    public override void Update(Group updatedEntity) => this.Name = updatedEntity.Name;
+}
---
+++ server/core/domain/Groups/IRepositoryGroup.cs
@@ -0,0 +1,5 @@
+﻿using LocadoraDeAutomoveis.Domain.Shared;
+
+namespace LocadoraDeAutomoveis.Domain.Groups;
+
+public interface IRepositoryGroup : IRepository<Group>;
