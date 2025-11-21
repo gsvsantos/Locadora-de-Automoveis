4cdaf653d943553fd6594d3068a3cd68f559f47e
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-20 23:57:11 -0300

    feat: Implementa endpoint para selecionar funcionário por id em EmployeeController.

--- server/web-api/Controllers/EmployeeController.cs
+++ server/web-api/Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using LocadoraDeAutomoveis.Application.Employees.Commands.Create;
 using LocadoraDeAutomoveis.Application.Employees.Commands.Delete;
 using LocadoraDeAutomoveis.Application.Employees.Commands.GetAll;
+using LocadoraDeAutomoveis.Application.Employees.Commands.GetById;
 using LocadoraDeAutomoveis.Application.Employees.Commands.SelfUpdate;
 using LocadoraDeAutomoveis.Application.Employees.Commands.Update;
 using LocadoraDeAutomoveis.Domain.Auth;
@@ -37,6 +38,22 @@ public class EmployeeController(IMediator mediator) : ControllerBase
         GetAllEmployeeRequest request = new();
 
         Result<GetAllEmployeeResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("get/{id:guid}")]
+    [Authorize("AdminPolicy")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        GetByIdEmployeeRequest request = new(id);
+
+        Result<GetByIdEmployeeResponse> result = await mediator.Send(request);
 
         if (result.IsFailed)
         {
