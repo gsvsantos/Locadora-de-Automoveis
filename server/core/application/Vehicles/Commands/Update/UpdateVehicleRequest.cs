72e5d6687a1923b1de01ea751ba9c8503ebfd1bc
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-22 16:19:11 -0300

    feat: Implementa handler para edição de veículos em /core/application.

---
+++ server/core/application/Vehicles/Commands/Update/UpdateVehicleRequest.cs
@@ -0,0 +1,29 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace LocadoraDeAutomoveis.Application.Vehicles.Commands.Update;
+
+public record UpdateVehicleRequestPartial(
+    string LicensePlate,
+    string Brand,
+    string Color,
+    string Model,
+    string FuelType,
+    int CapacityInLiters,
+    DateTimeOffset Year,
+    string? PhotoPath,
+    Guid GroupId
+) : IRequest<Result<UpdateVehicleResponse>>;
+
+public record UpdateVehicleRequest(
+    Guid Id,
+    string LicensePlate,
+    string Brand,
+    string Color,
+    string Model,
+    string FuelType,
+    int CapacityInLiters,
+    DateTimeOffset Year,
+    string? PhotoPath,
+    Guid GroupId
+) : IRequest<Result<UpdateVehicleResponse>>;
