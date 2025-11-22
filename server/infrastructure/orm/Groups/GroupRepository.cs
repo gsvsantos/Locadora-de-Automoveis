297cefd67f6b12aa2c3ce25a9aa2b241821f6e75
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-22 04:51:19 -0300

    feat: Implementa inclusão de veículos na seleção de grupos de veículos.

--- server/infrastructure/orm/Groups/GroupRepository.cs
+++ server/infrastructure/orm/Groups/GroupRepository.cs
@@ -1,7 +1,18 @@
 ﻿using LocadoraDeAutomoveis.Domain.Groups;
 using LocadoraDeAutomoveis.Infrastructure.Shared;
+using Microsoft.EntityFrameworkCore;
 
 namespace LocadoraDeAutomoveis.Infrastructure.Groups;
 
 public class GroupRepository(AppDbContext context)
-    : BaseRepository<Group>(context), IRepositoryGroup;
+    : BaseRepository<Group>(context), IRepositoryGroup
+{
+    public override Task<List<Group>> GetAllAsync() =>
+        this.records.Include(g => g.Vehicles).ToListAsync();
+
+    public override Task<List<Group>> GetAllAsync(int quantity) =>
+        this.records.Include(g => g.Vehicles).Take(quantity).ToListAsync();
+
+    public override Task<Group?> GetByIdAsync(Guid entityId) =>
+        this.records.Include(g => g.Vehicles).FirstOrDefaultAsync(g => g.Id.Equals(entityId));
+}
