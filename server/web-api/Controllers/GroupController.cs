c6b340a3db035f2e8992e877488aba7e49dce37a
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-22 06:10:26 -0300

    feat: Implementa endpoints de gerenciamento de grupos em GroupController.

---
+++ server/web-api/Controllers/GroupController.cs
@@ -0,0 +1,94 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Application.Groups.Commands.Create;
+using LocadoraDeAutomoveis.Application.Groups.Commands.Delete;
+using LocadoraDeAutomoveis.Application.Groups.Commands.GetAll;
+using LocadoraDeAutomoveis.Application.Groups.Commands.GetById;
+using LocadoraDeAutomoveis.Application.Groups.Commands.Update;
+using LocadoraDeAutomoveis.WebAPI.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeAutomoveis.WebApi.Controllers;
+
+[ApiController]
+[Route("api/group")]
+[Authorize("AdminOrEmployeePolicy")]
+public class GroupController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> Create([FromBody] CreateGroupRequest request)
+    {
+        Result<CreateGroupResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAll([FromQuery] GetAllGroupRequest? request)
+    {
+        request = request ?? new GetAllGroupRequest(null);
+
+        Result<GetAllGroupResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+        return Ok(result.Value);
+    }
+
+    [HttpGet("get/{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        GetByIdGroupRequest request = new(id);
+
+        Result<GetByIdGroupResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpPut("update/{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] UpdateGroupRequestPartial partialRequest)
+    {
+
+        UpdateGroupRequest request = new(
+            id,
+            partialRequest.Name
+        );
+
+        Result<UpdateGroupResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpDelete("delete/{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        DeleteGroupRequest request = new(id);
+
+        Result<DeleteGroupResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+}
