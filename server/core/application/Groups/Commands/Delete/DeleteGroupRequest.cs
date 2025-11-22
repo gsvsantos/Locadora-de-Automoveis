fa9ecf9b357757b16868b39a1aca4bc7a65b28d3
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-21 23:54:41 -0300

    feat: Implementa handler para exclusão de grupo de veículos em /core/application.

---
+++ server/core/application/Groups/Commands/Delete/DeleteGroupRequest.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace LocadoraDeAutomoveis.Application.Groups.Commands.Delete;
+
+public record DeleteGroupRequest(
+    Guid Id
+) : IRequest<Result<DeleteGroupResponse>>;
---
+++ server/core/application/Groups/Commands/Delete/DeleteGroupRequestHandler.cs
@@ -0,0 +1,46 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Application.Shared;
+using LocadoraDeAutomoveis.Domain.Groups;
+using LocadoraDeAutomoveis.Domain.Shared;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace LocadoraDeAutomoveis.Application.Groups.Commands.Delete;
+
+public class DeleteGroupRequestHandler(
+    IUnitOfWork unitOfWork,
+    IRepositoryGroup repositoryGroup,
+    ILogger<DeleteGroupRequestHandler> logger
+) : IRequestHandler<DeleteGroupRequest, Result<DeleteGroupResponse>>
+{
+    public async Task<Result<DeleteGroupResponse>> Handle(
+        DeleteGroupRequest request, CancellationToken cancellationToken)
+    {
+        Group? selectedGroup = await repositoryGroup.GetByIdAsync(request.Id);
+
+        if (selectedGroup is null)
+        {
+            return Result.Fail(ErrorResults.NotFoundError(request.Id));
+        }
+
+        try
+        {
+            await repositoryGroup.DeleteAsync(request.Id);
+
+            await unitOfWork.CommitAsync();
+
+            return Result.Ok(new DeleteGroupResponse());
+        }
+        catch (Exception ex)
+        {
+            await unitOfWork.RollbackAsync();
+
+            logger.LogError(
+                ex,
+                "An error occurred during the request. \n{@Request}.", request
+            );
+
+            return Result.Fail(ErrorResults.InternalServerError(ex));
+        }
+    }
+}
