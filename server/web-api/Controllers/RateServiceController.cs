203ec38e331f7ced1d6bd18f14cb93d5729c72b9
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-25 14:04:21 -0300

    feat: Implementa endpoints de gerenciamento do módulo de taxas e serviços.

---
+++ server/web-api/Controllers/RateServiceController.cs
@@ -0,0 +1,74 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Application.RateServices.Commands.Create;
+using LocadoraDeAutomoveis.Application.RateServices.Commands.Delete;
+using LocadoraDeAutomoveis.Application.RateServices.Commands.GetAll;
+using LocadoraDeAutomoveis.Application.RateServices.Commands.GetById;
+using LocadoraDeAutomoveis.WebAPI.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeAutomoveis.WebApi.Controllers;
+
+[ApiController]
+[Route("api/rate-service")]
+[Authorize("AdminOrEmployeePolicy")]
+public class RateServiceControllerI(Mediator mediator) : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> Create([FromBody] CreateRateServiceRequest request)
+    {
+        Result<CreateRateServiceResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAll([FromQuery] GetAllRateServiceRequest? request)
+    {
+        request = request ?? new GetAllRateServiceRequest(null);
+
+        Result<GetAllRateServiceResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+        return Ok(result.Value);
+    }
+
+    [HttpGet("get/{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        GetByIdRateServiceRequest request = new(id);
+
+        Result<GetByIdRateServiceResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpDelete("delete/{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        DeleteRateServiceRequest request = new(id);
+
+        Result<DeleteRateServiceResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+}
