208c515ffe7fc11722ddd3e5e44e0cbc007caa11
gsvsantos <gustavocnsantos02@gmail.com>
2025-11-30 13:09:26 -0300

    feat: Implementa módulo de aluguéis em /infrastructure/orm.

---
+++ server/infrastructure/orm/Rentals/RentalMapper.cs
@@ -0,0 +1,94 @@
+﻿using LocadoraDeAutomoveis.Domain.Rentals;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeAutomoveis.Infrastructure.Rentals;
+
+public class RentalMapper : IEntityTypeConfiguration<Rental>
+{
+    public void Configure(EntityTypeBuilder<Rental> builder)
+    {
+        builder.ToTable("Rentals");
+
+        builder.HasKey(r => r.Id);
+
+        builder.Property(r => r.StartDate)
+            .IsRequired();
+
+        builder.Property(r => r.ExpectedReturnDate)
+            .IsRequired();
+
+        builder.Property(r => r.ReturnDate)
+            .IsRequired(false);
+
+        builder.Property(r => r.StartKm)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(r => r.BaseRentalPrice)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(r => r.GuaranteeValue)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(r => r.FinalPrice)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(r => r.EstimatedKilometers)
+            .IsRequired(false);
+
+        builder.Property(r => r.Status)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Property(r => r.SelectedPlanType)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.HasOne(r => r.Client)
+            .WithMany()
+            .HasForeignKey(r => r.ClientId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(r => r.Driver)
+            .WithMany()
+            .HasForeignKey(r => r.DriverId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(r => r.Vehicle)
+            .WithMany()
+            .HasForeignKey(r => r.VehicleId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(r => r.PricingPlan)
+            .WithMany()
+            .HasForeignKey(r => r.PricingPlanId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(r => r.Employee)
+            .WithMany()
+            .HasForeignKey(r => r.EmployeeId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(r => r.RateServices)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired(false);
+
+        builder.HasOne(t => t.Tenant)
+            .WithMany()
+            .HasForeignKey(t => t.TenantId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(u => u.User)
+            .WithMany()
+            .HasForeignKey(g => g.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(f => new { f.TenantId, f.UserId, f.IsActive });
+    }
+}
---
+++ server/infrastructure/orm/Rentals/RentalRepository.cs
@@ -0,0 +1,45 @@
+﻿using LocadoraDeAutomoveis.Domain.Rentals;
+using LocadoraDeAutomoveis.Infrastructure.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeAutomoveis.Infrastructure.Rentals;
+
+public class RentalRepository(AppDbContext context)
+    : BaseRepository<Rental>(context), IRepositoryRental
+{
+    public override async Task<List<Rental>> GetAllAsync()
+    {
+        return await this.records
+            .Include(r => r.Employee)
+            .Include(r => r.Client)
+            .Include(r => r.Driver)
+            .Include(r => r.Vehicle)
+            .Include(r => r.PricingPlan)
+            .Include(r => r.RateServices)
+            .ToListAsync();
+    }
+
+    public override async Task<List<Rental>> GetAllAsync(int quantity)
+    {
+        return await this.records
+            .Include(r => r.Employee)
+            .Include(r => r.Client)
+            .Include(r => r.Driver)
+            .Include(r => r.Vehicle)
+            .Include(r => r.PricingPlan)
+            .Include(r => r.RateServices)
+            .Take(quantity).ToListAsync();
+    }
+
+    public override async Task<Rental?> GetByIdAsync(Guid entityId)
+    {
+        return await this.records
+            .Include(r => r.Employee)
+            .Include(r => r.Client)
+            .Include(r => r.Driver)
+            .Include(r => r.Vehicle)
+            .Include(r => r.PricingPlan)
+            .Include(r => r.RateServices)
+            .FirstOrDefaultAsync(d => d.Id.Equals(entityId));
+    }
+}
