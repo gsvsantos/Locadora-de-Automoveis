a2236541707cdcc8f5ccf0a4ac2e5bf4d106b3e3
gsvsantos <gustavocnsantos02@gmail.com>
2025-12-01 00:28:39 -0300

    feat: Implementa validação para devoluções de aluguel.

---
+++ server/core/application/Validators/RentalReturnValidators.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using LocadoraDeAutomoveis.Domain.Rentals;
+
+namespace LocadoraDeAutomoveis.Application.Validators;
+
+public class RentalReturnValidator : AbstractValidator<RentalReturn>
+{
+    public RentalReturnValidator()
+    {
+        RuleFor(r => r.ReturnDate)
+            .NotEmpty().WithMessage("The Return Date is required.")
+            .GreaterThan(r => r.Rental.StartDate)
+            .WithMessage("The Return Date must be greater than the Start Date.");
+
+        RuleFor(r => r.EndKm)
+            .GreaterThan(0)
+            .WithMessage("The final odometer reading must be greater than zero.");
+
+        RuleFor(r => r.TotalMileage)
+            .GreaterThan(0)
+            .WithMessage("The total mileage must be greater than zero.");
+
+        RuleFor(r => r.FuelLevelAtReturn)
+            .IsInEnum()
+            .WithMessage("The Fuel Level is invalid.");
+    }
+}
