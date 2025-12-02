120fb12dc75d7062d6066c7f21bd6dbd7077ac47
gsvsantos <gustavocnsantos02@gmail.com>
2025-12-02 12:47:10 -0300

    feat: Implementa controllers com endpoints para gerenciamento de cupons e parceiros.

---
+++ server/web-api/Controllers/CouponController.cs
@@ -0,0 +1,66 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Application.Coupons.Commands.Create;
+using LocadoraDeAutomoveis.Application.Coupons.Commands.Delete;
+using LocadoraDeAutomoveis.Application.Coupons.Commands.Update;
+using LocadoraDeAutomoveis.WebAPI.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeAutomoveis.WebApi.Controllers;
+
+[ApiController]
+[Route("api/coupon")]
+[Authorize("AdminOrEmployeePolicy")]
+public class CouponController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> Create([FromBody] CreateCouponRequest request)
+    {
+        Result<CreateCouponResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpPut("update/{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] UpdateCouponRequestPartial partialRequest)
+    {
+
+        UpdateCouponRequest request = new(
+            id,
+            partialRequest.Name,
+            partialRequest.DiscountValue,
+            partialRequest.ExpirationDate,
+            partialRequest.PartnerId
+        );
+
+        Result<UpdateCouponResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpDelete("delete/{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        DeleteCouponRequest request = new(id);
+
+        Result<DeleteCouponResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+}
---
+++ server/web-api/Controllers/PartnerController.cs
@@ -0,0 +1,94 @@
+﻿using FluentResults;
+using LocadoraDeAutomoveis.Application.Partners.Commands.Create;
+using LocadoraDeAutomoveis.Application.Partners.Commands.Delete;
+using LocadoraDeAutomoveis.Application.Partners.Commands.GetAll;
+using LocadoraDeAutomoveis.Application.Partners.Commands.GetCoupons;
+using LocadoraDeAutomoveis.Application.Partners.Commands.Update;
+using LocadoraDeAutomoveis.WebAPI.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeAutomoveis.WebApi.Controllers;
+
+[ApiController]
+[Route("api/partner")]
+[Authorize("AdminOrEmployeePolicy")]
+public class PartnerController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> Create([FromBody] CreatePartnerRequest request)
+    {
+        Result<CreatePartnerResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAll([FromQuery] GetAllPartnerRequest? request)
+    {
+        request = request ?? new GetAllPartnerRequest(null);
+
+        Result<GetAllPartnerResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+        return Ok(result.Value);
+    }
+
+    [HttpGet("get/{id:guid}")]
+    public async Task<IActionResult> GetCoupons(Guid id)
+    {
+        GetCouponsPartnerRequest request = new(id);
+
+        Result<GetCouponsPartnerResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpPut("update/{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] UpdatePartnerRequestPartial partialRequest)
+    {
+
+        UpdatePartnerRequest request = new(
+            id,
+            partialRequest.FullName
+        );
+
+        Result<UpdatePartnerResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+
+    [HttpDelete("delete/{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        DeletePartnerRequest request = new(id);
+
+        Result<DeletePartnerResponse> result = await mediator.Send(request);
+
+        if (result.IsFailed)
+        {
+            return this.MapFailure(result.ToResult());
+        }
+
+        return Ok(result.Value);
+    }
+}
