faf368a0e880ba5451c45435e6e2cdd0026fd006
gsvsantos <gustavocnsantos02@gmail.com>
2025-12-05 11:37:49 -0300

    feat: Implementa Transloco para internacionalização (i18n).

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
-};
+    provideTransloco({
+      config: {
+        availableLangs: ['en-US'],
+        defaultLang: 'en-US',
+        fallbackLang: 'en-US',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
