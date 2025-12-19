f60d1d2dbb7d819c0521a91af5d8880741106da1
gsvsantos <gustavocnsantos02@gmail.com>
2025-12-19 15:02:52 -0300

    feat: Implementa resolver para detalhes do veículo disponível.

--- web-portal/src/app/resolvers/vehicle.resolvers.ts
+++ web-portal/src/app/resolvers/vehicle.resolvers.ts
@@ -2,8 +2,8 @@ import { inject } from '@angular/core';
 import { ResolveFn, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { VehicleService } from '../services/vehicle.service';
-import { PagedResult } from '../models/paged-result.model';
-import { Vehicle } from '../models/vehicle.model';
+import { PagedResult } from '../models/paged-result.models';
+import { Vehicle } from '../models/vehicle.models';
 
 export const availableVehiclesResolver: ResolveFn<PagedResult<Vehicle>> = (
   route: ActivatedRouteSnapshot,
@@ -17,4 +17,14 @@ export const availableVehiclesResolver: ResolveFn<PagedResult<Vehicle>> = (
   const fuelType = route.queryParamMap.get('fuelType') || undefined;
 
   return vehicleService.getAvailableVehicles(page, pageSize, term, groupId, fuelType);
+};
+
+export const vehicleDetailsResolver: ResolveFn<Vehicle> = (route: ActivatedRouteSnapshot) => {
+  const vehicleService: VehicleService = inject(VehicleService);
+
+  if (!route.paramMap.has('id')) throw new Error('Missing "ID" parameter.');
+
+  const id: string = route.paramMap.get('id')!;
+
+  return vehicleService.getById(id);
 };
