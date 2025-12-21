17388739d894c841af745d67f8eb3f360313fd33
gsvsantos <gustavocnsantos02@gmail.com>
2025-12-21 20:05:53 -0300

    fix: Corrige scripts para incluir nova Env ao ambiente de produção.

--- web-admin/scripts/write-environment-prod.js
+++ web-admin/scripts/write-environment-prod.js
@@ -11,12 +11,14 @@ function requireEnv(variableName) {
 
 const apiUrl = requireEnv('APIURL');
 const googleClientId = requireEnv('CLIENT_ID');
+const captcha_key = requireEnv('CAPTCHA_KEY');
 
 const environmentFileContent =
   `export const environment = {\n` +
   `  production: true,\n` +
   `  apiUrl: ${JSON.stringify(apiUrl)},\n` +
   `  client_id: ${JSON.stringify(googleClientId)}\n` +
+  `  captcha_key: ${JSON.stringify(captcha_key)}\n` +
   `} as const;\n`;
 
 const environmentFilePath = path.join(__dirname, '..', 'src', 'environments', 'environment.ts');
